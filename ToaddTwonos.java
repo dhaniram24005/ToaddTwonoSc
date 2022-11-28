bcc5561d98d94458a7e5f0858a23f9b166696e51
dhaniram24005 <87693749+dhaniram24005@users.noreply.github.com>
2022-11-28 12:31:59 +0530

    Add files via upload

---
+++ ToaddTwonos.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class ToaddTwonos {
+    public static void main(String args[]) {
+
+        int num1, num2, sum;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter th first no:");
+        num1 = sc.nextInt();
+
+        System.out.println("Enter the second no :");
+        num2 = sc.nextInt();
+
+        sum = num1 + num2;
+        System.out.println("The sum of the two no is : " + sum);
+    }
+
+}
